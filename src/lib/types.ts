b2b36b6db785023d2dbf29d6c2caa46e3b8ba9e7
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-08-24 22:06:12 +0900

    feat: expose latest editorial experiment

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -80,6 +80,9 @@ export interface DashboardBlog extends Blog {
   searchCtrPercent: number | null;
   searchPosition: number | null;
   topSearchQuery: string | null;
+  latestExperimentAxis: string | null;
+  latestExperimentVariant: string | null;
+  latestExperimentHypothesis: string | null;
   recentRuns: number;
   failedRuns: number;
 }
